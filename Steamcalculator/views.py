cbd71311c556bd8da37439d2cfe4a53110f0a49d
Juan Rodriguez Lacasa <juanrl1997@gmail.com>
2019-04-01 16:57:35 +0200

    temperature checker

--- Steamcalculator/views.py
+++ Steamcalculator/views.py
@@ -11,18 +11,13 @@ from SatCalc import SatCalcP
 from unSatCalc import unSatCalcP
 from unSatCalc import unSatCalcT
 
-from plots import Pvplot
-from plots import Phplot
-from plots import Tsplot
-from plots import Hsplot
+from allplot import allplot
+
+from check import checkinput2T
+from check import Input1range
 
 import numpy as np
 import pandas as pd
-
-from io import BytesIO
-import base64
-import matplotlib.pyplot as plt
-import numpy as np
 
 
 def home(request):
@@ -32,33 +27,19 @@ def home(request):
 
 def count(request):
 
-
-
     values=[0,0,0,0,0,0,0,0,0]
-
 
 # Input 1: Pressure or Temperature
     Input1=str(request.GET['Input1'])
-
 # Input 2: volume, enthalpy, entropy...
     Input2=str(request.GET['Input2'])
 
-    Input1value = request.GET['Input1value']
-    Input2value = request.GET['Input2value']
-
-    print(Input2)
-
-    print(Input2value)
-
-    #state=Presssat(Input1value,Input2value,Input2)
-    #values=SatCalcT(Input1value,Input2value,Input2)
+    Input1value = float(request.GET['Input1value'])
+    Input2value = float(request.GET['Input2value'])
 
     if Input1=='Temperature':
         state=Tempsat(Input1value,Input2value,Input2)
-        print(state)
         if state=='saturated':
-            print('run')
-
             values=SatCalcT(Input1value,Input2value,Input2)
         elif state=='unsaturated':
             values=unSatCalcT(Input1value,Input2value,Input2)
@@ -69,60 +50,20 @@ def count(request):
             values=SatCalcP(Input1value,Input2value,Input2)
         elif state=='unsaturated':
             values=unSatCalcP(Input1value,Input2value,Input2)
+
     Pvalue=values[0]
     Tvalue=values[1]
     vvalue=values[2]
     hvalue=values[4]
     svalue=values[5]
 
+    [graphic,graphic2, graphic3, graphic4]= allplot(Pvalue,Tvalue,vvalue,hvalue,svalue)
 
-# plot 1
-    Pvplot(Pvalue,vvalue)
-
-
-    buffer = BytesIO()
-    plt.savefig(buffer, format='png')
-    buffer.seek(0)
-    image_png = buffer.getvalue()
-    buffer.close()
-
-    graphic = base64.b64encode(image_png)
-    graphic = graphic.decode('utf-8')
-# plot 2
-    Phplot(Pvalue,hvalue)
-
-    buffer = BytesIO()
-    plt.savefig(buffer, format='png')
-    buffer.seek(0)
-    image_png = buffer.getvalue()
-    buffer.close()
-
-    graphic2 = base64.b64encode(image_png)
-    graphic2 = graphic2.decode('utf-8')
-# plot 3
-    Tsplot(Tvalue,svalue)
-
-    buffer = BytesIO()
-    plt.savefig(buffer, format='png')
-    buffer.seek(0)
-    image_png = buffer.getvalue()
-    buffer.close()
-
-    graphic3 = base64.b64encode(image_png)
-    graphic3 = graphic3.decode('utf-8')
-
-# plot 4
-    Hsplot(hvalue,svalue)
-
-    buffer = BytesIO()
-    plt.savefig(buffer, format='png')
-    buffer.seek(0)
-    image_png = buffer.getvalue()
-    buffer.close()
-
-    graphic4 = base64.b64encode(image_png)
-    graphic4 = graphic4.decode('utf-8')
-
+    error=False
+    if error==False:
+        error=Input1range(Input1,Input1value)
+    if error==False:
+        error=checkinput2T(Input1value,Input2,Input2value)
 
     return render(request,'count.html',{
     'state':state,
@@ -138,6 +79,7 @@ def count(request):
     'graphic':graphic,
     'graphic2':graphic2,
     'graphic3':graphic3,
-    'graphic4':graphic4
+    'graphic4':graphic4,
+    'error':error
 
     })
