690db47ea5acfd3be158bf4040a9839d7a8d57ed
Juan Rodriguez Lacasa <juanrl1997@gmail.com>
2019-04-09 13:06:45 +0200

    Static root

--- Steamcalculator/settings.py
+++ Steamcalculator/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
@@ -124,5 +124,5 @@ STATIC_DIRS = 'static'
 
 STATICFILES_DIRS = [
     STATIC_DIRS,
-    
+
 ]
